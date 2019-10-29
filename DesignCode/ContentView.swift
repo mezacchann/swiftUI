029e6ccd1f6422630cdd2b1dd315edd0294e91fd
SergeiMeza <sergeimeza@gmail.com>
2019-10-29 16:26:24 +0900

    Initial Commit

---
+++ DesignCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by Meza Rodriguez Jeany Sergei on 2019/10/29.
+//  Copyright © 2019 Meza Rodriguez Jeany Sergei. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
